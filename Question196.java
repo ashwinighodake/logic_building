2adf47b2c98b037c6d59d8b563df3addd0134cbe
ashwinighodake <ashwini752000@gmail.com>
2021-12-01 10:21:24 +0530

    Write a program which accept number from user and return multiplication of all digits.

--- Question196.java
+++ Question196.java
@@ -1,10 +1,11 @@
 /*
-Write a program which accept number from user and return the count of digits in between 3 and 7.
+Write a program which accept number from user and return
+multiplication of all digits.
 */
 
 import java.util.*;
 
-class Question195
+class Question196
 {
     public static void main(String args[])
     {
@@ -25,17 +26,18 @@ class  Range
 {
     public int RangeX(int iNo)
     {
-         int iDigit=0,i=0;
+         int iDigit=0,i=0,iMul=1;
 
+        if(iNo==0)
+        {
+            return 0;
+        }
          while(iNo!=0)
          {
              iDigit=iNo%10;
-             if((iDigit>3)&&(iDigit<7))
-             {
-                 i++;
-             }
+             iMul=iMul*iDigit;
              iNo=iNo/10;
          }
-         return i;
+         return iMul;
     }
 }
